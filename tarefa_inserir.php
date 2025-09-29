9df68a00171a16aa6df705595fcd51fd9aae2120
Midlejvbm <pedrovbm6@gmail.com>
2025-09-29 10:33:44 -0300

    Inclusão do arquivo tarefa_inserir

---
+++ tarefa_inserir.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarefa :: inserir </title>
+    <?php
+    include "referencias.php";
+    ?>
+</head>
+<body>
+    <h1> Tarefa inserida</h1>
+    
+</body>
+</html>
