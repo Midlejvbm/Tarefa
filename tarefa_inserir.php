caae62a281c6af8b76daaa8f47ffc7eee37f4c56
Midlejvbm <pedrovbm6@gmail.com>
2025-10-06 10:39:58 -0300

    Finalizando o inserir

--- tarefa_inserir.php
+++ tarefa_inserir.php
@@ -10,6 +10,34 @@
 </head>
 <body>
     <h1> Tarefa inserida</h1>
+
+    <?php
+      $descricao = $_POST["txtDescricao"];
+      $data_entrega = $_POST["txtData"];
+      $prioridade = $_POST["txtPrioridade"];
+      $responsavel = $_POST["txtresponsavel"];
+      
+      $sql = "INSERT INTO tarefa(descricao,data_entrega,prioridade,responsavel) VALUES (?,?,?,?)";
+
+      $comando = $conexao->prepare($sql);
+      
+      $comando->bind_param("ssss",$descricao,$data_entrega,$prioridade,$responsavel);
+
+      if ($comando->execute())
+      {
+        echo "<h1> Tarefa marcada!</h1>";
+      }
+      else
+      {
+        echo "<h1>Erro!confira os dados!</h1>";
+      }
+
+      
+
+
+
+
+    ?>
     
 </body>
 </html>
